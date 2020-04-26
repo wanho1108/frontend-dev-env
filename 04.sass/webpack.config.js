27fc0e1b45fab0a9db88d0a2ea21fbf2b787dcfa
wanho1108 <wanho1108@gmail.com>
2020-04-27 05:40:02 +0900

    사스 사용법

---
+++ 04.sass/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './app.js'
+  },
+  output: {
+    path: path.resolve('./dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+}
